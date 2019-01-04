4ca48457e0c5cd2619e0d0013b253de9d5ed1ae0
TeamK <koceto1973@yahoo.ca>
2019-01-04 02:38:31 +0200

    mongoose in

---
+++ server/server.js
@@ -0,0 +1,53 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    required: true, // validator
+    minlength: 1,   // validator
+    trim: true		// remove leading and trailing zeros
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,    // type
+    default: null	 // defaults
+  }
+});
+
+// var otherTodo = new Todo({
+//   text: 'Something to do'
+// });
+//
+// otherTodo.save().then((doc) => {
+//   console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//   console.log('Unable to save', e);
+// });
+
+// User
+// email - require it - trim it - set type - set min length of 1
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
+});
+
+var user = new User({
+  email: 'andrew@example.com   '
+});
+
+user.save()
+.then((doc) => {
+  console.log('User saved', doc);
+}, (e) => {
+  console.log('Unable to save user', e);
+});
