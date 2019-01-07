8a4b7f0b08eb763c070b6bed85a4992f9c3a8d22
TeamK <koceto1973@yahoo.ca>
2019-01-07 10:35:41 +0200

    user model and /users route in

--- server/server.js
+++ server/server.js
@@ -97,6 +97,23 @@ app.patch('/todos/:id', (req, res) => {
 
 });
 
+// POST /users
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body); // email validator runs here
+
+  user
+  .save()
+  .then((user) => {
+    res.send(user);
+  })
+  .catch((e) => {
+    res
+    .status(400)
+    .send(e);
+  })
+});
+
 app.listen(port, () => {
   console.log(`Started at port ${port}`);
 });
